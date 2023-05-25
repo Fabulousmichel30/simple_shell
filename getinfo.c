52f51c3d87b6530d6d302d92f729853ea4bf7c44
samueladdoenchill <samueladdoenchill@gmail.com>
2023-05-24 21:01:40 -0700

    added getinfo.c

--- getinfo.c
+++ getinfo.c
@@ -1,8 +1,10 @@
 #include "shell.h"
 
 /**
- * clear_info - Initializes the info_t struct.
- * @info: Pointer to the info_t struct.
+ * clear_info - Initializes info_t struct
+ * @info: Pointer to the info_t struct
+ *
+ * Initializes the fields of the info_t struct to their default values.
  */
 void clear_info(info_t *info)
 {
@@ -13,13 +15,16 @@ void clear_info(info_t *info)
 }
 
 /**
- * set_info - Initializes the info_t struct.
- * @info: Pointer to the info_t struct.
- * @av: Argument vector.
+ * set_info - Initializes info_t struct
+ * @info: Pointer to the info_t struct
+ * @av: Argument vector
+ *
+ * Initializes fields of info_t struct using the provided argument vector.
+ * It also performs alias and variable replacement on the argument string.
  */
 void set_info(info_t *info, char **av)
 {
-	int i;
+	int i = 0;
 
 	info->fname = av[0];
 	if (info->arg)
@@ -34,9 +39,12 @@ void set_info(info_t *info, char **av)
 				info->argv[1] = NULL;
 			}
 		}
-		for (i = 0; info->argv && info->argv[i]; i++)
-			;
-		info->argc = i;
+		else
+		{
+			for (i = 0; info->argv[i]; i++)
+				;
+			info->argc = i;
+		}
 
 		replace_alias(info);
 		replace_vars(info);
@@ -44,32 +52,44 @@ void set_info(info_t *info, char **av)
 }
 
 /**
- * free_info - Frees the fields of the info_t struct.
- * @info: Pointer to the info_t struct.
- * @all: True if freeing all fields.
+ * free_info - Frees info_t struct fields
+ * @info: Pointer to the info_t struct
+ * @all: True if freeing all fields
+ *
+ * Frees the dynamically allocated fields of the info_t struct.
+ * If `all` is true, it also frees additional fields and performs cleanup.
  */
 void free_info(info_t *info, int all)
 {
+	ffree(info->argv);
+	info->argv = NULL;
+	info->path = NULL;
+
 	if (all)
 	{
 		if (!info->cmd_buf)
 			free(info->arg);
-		if (info->env)
-			free_list(&(info->env));
-		if (info->history)
-			free_list(&(info->history));
-		if (info->alias)
-			free_list(&(info->alias));
+		info->arg = NULL;
+
+		free_list(&(info->env));
+		info->env = NULL;
+
+		free_list(&(info->history));
+		info->history = NULL;
+
+		free_list(&(info->alias));
+		info->alias = NULL;
+
 		ffree(info->environ);
 		info->environ = NULL;
+
 		bfree((void **)info->cmd_buf);
+		info->cmd_buf = NULL;
+
 		if (info->readfd > 2)
 			close(info->readfd);
+
 		_putchar(BUF_FLUSH);
 	}
-
-	ffree(info->argv);
-	info->argv = NULL;
-	info->path = NULL;
 }
 
